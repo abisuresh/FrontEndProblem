8a61d6fa3c0fe17bc26cf96ff544b20f9a7eee7c
Abbi Devins-Suresh <aesther2012@gmail.com>
2024-05-16 16:16:29 -0400

    adding in routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PoetComponent} from "./poet/poet.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'poet-component', component: PoetComponent }
+];
